33a37fef983ffeb406fd0dc49a1a599534998115
wetrakat <hogdogcar@gmail.com>
2024-10-07 01:46:09 +0200

    add cp subsolution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const spawnChildProcess = async (args) => {
+  const scriptPath = join(__dirname, 'files', 'script.js');
+  const childProcess = spawn('node', [scriptPath, ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.setEncoding('utf8');
+  childProcess.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Dog', 'Cat']);
