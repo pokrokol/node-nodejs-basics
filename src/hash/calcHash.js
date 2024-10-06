88ca5137110979e8bb1b98f41bdee6de97bc4bd0
wetrakat <hogdogcar@gmail.com>
2024-10-07 01:23:58 +0200

    add hash and streams subsolution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const calculateHash = async () => {
+  try {
+    const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+    const content = await readFile(filePath, 'utf8');
+    const hash = createHash('sha256').update(content).digest('hex');
+    console.log(hash);
+  } catch (error) {
+    console.error('Error calculating hash:', error);
+  }
 };
 
-await calculateHash();
+calculateHash();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
-const write = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { stdin } from 'node:process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const write = async () => {
+  const filePath = `${__dirname}/files/fileToWrite.txt`;
+  const writable = fs.createWriteStream(filePath);
+  stdin.pipe(writable);
 };
 
-await write();
+write();
