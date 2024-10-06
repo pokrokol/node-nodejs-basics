be162d0c6d9dc09c19c27dc6bec7157d799102fc
wetrakat <hogdogcar@gmail.com>
2024-10-07 01:33:18 +0200

    add zip and workers subsolution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCount = cpus().length;
+  let num = 10;
+
+  const workerPromises = Array.from({ length: cpuCount }, () => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(join(__dirname, 'worker.js'), {
+        workerData: ++num,
+      });
+
+      worker.on('message', resolve);
+      worker.on('error', reject);
+    });
+  });
+
+  const results = await Promise.allSettled(workerPromises);
+
+  const formattedResults = results.map(({ status, value }) => ({
+    status: status === 'fulfilled' ? 'resolved' : 'error',
+    data: status === 'fulfilled' ? value : null,
+  }));
+
+  console.log(formattedResults);
 };
 
-await performCalculations();
+performCalculations();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
-const decompress = async () => {
-    // Write your code here 
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const inputFilePath = path.join(__dirname, 'files', 'archive.gz');
+const outputFilePath = path.join(__dirname, 'files', 'fileToDecompress.txt');
+
+export const decompressFile = async () => {
+  const unzipStream = createUnzip();
+  const readStream = createReadStream(inputFilePath);
+  const writeStream = createWriteStream(outputFilePath);
+  
+  readStream.pipe(unzipStream).pipe(writeStream);
 };
 
-await decompress();
+decompressFile();
