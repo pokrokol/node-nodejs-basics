ca02be5680b5ccc9d1291f435a95f6871c0778d1
wetrakat <hogdogcar@gmail.com>
2024-10-06 14:33:00 +0200

    complete fs subtask

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
-const read = async () => {
-    // Write your code here 
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const read = async () => {
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const data = await readFile(filePath, { encoding: 'utf8' });
+    console.log(data);
+  } catch (error) {
+    console.error('FS operation failed', error);
+    throw new Error('FS operation failed');
+  }
 };
 
-await read();
+read();
